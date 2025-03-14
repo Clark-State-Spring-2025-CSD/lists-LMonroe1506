77cbb3b9c8196a17f1a9fea6166aa5fdff2a65e7
LMonroe1506 <monroelois1506@gmail.com>
2025-03-14 17:11:28 -0400

    Update duelinglists.py

--- duelinglists.py
+++ duelinglists.py
@@ -18,3 +18,37 @@
 #Player two's highest number is 8 at index 1
 #Player one's lowest number is 1 at index 4
 #Player two's lowest number is 1 at index 5
+
+import random
+
+player_one = [random.randint(1,50) for i in range(10)]
+player_two = [random.randint(1,50) for i in range(10)]
+
+player_one_wins = 0
+player_two_wins = 0
+
+for p1, p2 in zip(player_one, player_two):
+    if p1 > p2:
+        player_one_wins += 1
+    elif p2 > p1:
+        player_two_wins += 1
+
+player_one_highest = max(player_one)
+player_one_highest_index = player_one.index(player_one_highest)
+player_one_lowest = min(player_one)
+player_one_lowest_index = player_one.index(player_one_lowest)
+
+player_two_highest = max(player_two)
+player_two_highest_index = player_two.index(player_two_highest)
+player_two_lowest = min(player_two)
+player_two_lowest_index = player_two.index(player_two_lowest)
+
+print(f"Player One = {player_one}")
+print(f"Player Two = {player_two}")
+
+print(f"Player one won {player_one_wins} times")
+print(f"Player two won {player_two_wins} times")
+print(f"Player one's highest number is {player_one_highest} at index {player_one_highest_index}")
+print(f"Player two's highest number is {player_two_highest} at index {player_two_highest_index}")
+print(f"Player one's lowest number is {player_one_lowest} at index {player_one_lowest_index}")
+print(f"Player two's lowest number is {player_two_lowest} at index {player_two_lowest_index}")
